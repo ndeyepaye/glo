77f2e1fe4211f35aa4d88101fa7073cfaf255f04
ndeyepaye <mamipaye@gmail.com>
2019-09-30 00:31:06 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,4 @@
-
+docker stop mongo_devoir1
+docker system prune -f
+docker run -d --name mongo_devoir1 mongo:4.2
+docker exec -it mongo_devoir1 mongo ds349587.mlab.com:49587/glo4035-datasets -u etudiant -p maison2000
